05f67b7d0514323470d8e4ef9da660b192e837d7
chrisjohnsonSBU <127891930+chrisjohnsonSBU@users.noreply.github.com>
2024-12-16 11:52:01 -0500

    Updates for quick file functions

--- src/codechembook/quickTools.py
+++ src/codechembook/quickTools.py
@@ -1,22 +1,20 @@
 # some tools for handlign opening and plotting....
 import numpy as np
 from codechembook import quickplots as qp
-from symbols import math, typography
 
-
-'''
-This function is going to read from a csv quickly. 
-The base behavior is to just take a number of columns and read them and return them
-'''
 
 def quickOpenCSV(file, cols = None, delimiter = ",", skip_header = 1):
+    '''
+    This function is going to read from a csv quickly. 
+    The base behavior is to just take a number of columns and read them and return them
+    '''
     if cols == None: # no specific set of columns specified
         read_columns = np.genfromtxt(file, 
                                     delimiter = delimiter,
                                     skip_header=skip_header,
                                     unpack=True,
                                     )
-    else: # probably shoudl check to make sure it is a thing that can be a list... in elif
+    else: # probably should check to make sure it is a thing that can be a list... in elif
         read_columns = np.genfromtxt(file, 
                                 delimiter = delimiter,
                                 skip_header=skip_header,
@@ -25,27 +23,62 @@ def quickOpenCSV(file, cols = None, delimiter = ",", skip_header = 1):
                                 )
     return read_columns
 
-def quickSaveCSV(file, data = data, delimiter = ', ', header = None):
+def quickSaveCSV(file, data, format = None, delimiter = ', ', header = ''):
+    # Check which type of data we have received and make a new numpy array holding it
     if isinstance(data, list) or isinstance(data, tuple):
+        # If we got a list, then we need to turn rows into columns to make a 2D array
         prep_data = np.column_stack(data)
+
     elif isinstance(data, np.ndarray):
-        if len(shape(data)) > 2:
+        # If we got a numpy array, we need to figure out what its dimension is
+        if len(data.shape) > 2:
             print('This function can not handle arrays with three or more dimensions!')
             return
-        elif len(shape(data)) == 2:
+
+        elif len(data.shape) in [2, 1]:
+            # Easy, we just copy it over
             prep_data = data
-        elif len(shape(data)) == 1:
-            prep_data = data
+
+        # elif len(data.shape) == 1:
+        #     prep_data = data
         else:
             print('Incompatible numpy array shape for this function.')
     elif isinstance(data, (int, float, complex, bool, str)):
-        prep_data = data
+        prep_data = np.array([data])
     else:
         print('This type is not supported by this function.')
         return
-    
-    
 
+    if format is None:
+        new_format = '%.14e'
+    elif isinstance(format, str):
+        if format[0] == '%' or format[1] == '%':
+            # We have an old-school python format string, use it as is
+            new_format = format
+
+        else:
+            # Assume we have an f-strings type format string
+            def temp(x):
+                return f'{x:{format}}'
+            fast_format = np.vectorize(temp)
+            prep_data = fast_format(prep_data)
+            new_format = '%s'
+    elif isinstance(format, list):
+        if len(format) == prep_data.shape[1]:
+            string_data = []
+            print(f'Formatting {prep_data.shape[1]} columns with {len(format)} format statements')
+            for i, f in enumerate(format):
+                def temp(x):
+                    return f'{x:{f}}'
+                fast_format = np.vectorize(temp)
+                string_data.append(fast_format(prep_data[:,i]))
+            prep_data = np.column_stack(string_data)
+            new_format = '%s'
+        else:
+            print(f'Failed: Received {prep_data.shape[1]} columns but {len(format)} format statements.\nNot writing any file.')
+            return
+
+    np.savetxt(file, prep_data, delimiter = delimiter, fmt = new_format, header = header, comments = '')
 
 def quickPlotCSV(file, cols = None, skip_header = 1, plotType = "scatter", xcol = 0):
     '''
@@ -68,7 +101,7 @@ def quickPlotCSV(file, cols = None, skip_header = 1, plotType = "scatter", xcol 
 
     return fig
 
-def quickGetFileNames(title="Select files to open", initialdir='.', filetypes=(('All files', '*.*'))):
+def quickOpenFilenames(title="Select files to open", initialdir='.', filetypes='All files, *.*', sorted = True):
     """
     Opens a file dialog to select multiple files, returning a sorted list of Path objects.
     
@@ -82,32 +115,55 @@ def quickGetFileNames(title="Select files to open", initialdir='.', filetypes=((
     Returns:
     - List[Path]: A sorted list of selected file paths as Path objects.
     """
+    from PyQt5.QtWidgets import QApplication, QFileDialog
+
+    app = QApplication([])  # Create a Qt application
+    filepaths, _ = QFileDialog.getOpenFileNames(None, title, initialdir, filetypes)
+
+    if sorted:
+        filepaths = sorted(filepaths)
+
+    return filepaths
+
+def quickOpenFilename(title="Select files to open", initialdir='.', filetypes='All files, *.*'):
+    """
+    Opens a file dialog to select multiple files, returning a sorted list of Path objects.
     
-    import tkinter as tk
-    from tkinter import filedialog
-    from pathlib import Path
+    Parameters:
+    - title (str): The title of the file selection dialog window. Defaults to "Select files to open".
+    - initialdir (str or Path): The initial directory for the dialog. Defaults to the current directory '.'.
+                               Accepts both Path objects and strings.
+    - filetypes (tuple): The types of files to display in the dialog. Defaults to showing all files.
+                         Should be a tuple in the format (('Description', '*.extension')).
+    
+    Returns:
+    - Path: A Path object.
+    """
+    return quickOpenFilenames(title = title, initialdir = initialdir, filetypes = filetypes)[0]
 
-    # Create a root window but keep it minimized and on top
-    root = tk.Tk()
-    root.lift()  # Ensure it's on top
-    root.attributes('-topmost', True)  # Force window to be topmost
-    root.withdraw()  # Hide the root window from view
+def quickSaveFilename(title="Choose or create a filename to save", initialdir='.', filetypes='All files*.*'):
+    """
+    Opens a file dialog to choose a filename for saving, returns a Path object.
+    
+    Parameters:
+    - title (str): The title of the file selection dialog window. Defaults to "Define file to save".
+    - initialdir (str or Path): The initial directory for the dialog. Defaults to the current directory '.'.
+                               Accepts both Path objects and strings.
+    - filetypes (tuple): The types of files to display in the dialog. Defaults to showing all files.
+                         Should be a tuple in the format (('Description', '*.extension')).
+    
+    Returns:
+    - Path: the path to the filename that is to be saved.
+    """
+    from PyQt5.QtWidgets import QApplication, QFileDialog
 
-    # Ensure window becomes visible and waits before proceeding
-    root.deiconify()  # Bring the window back to life but hidden
-    root.wait_visibility()  # Wait for the window to become visible
-    root.attributes('-topmost', True)  # Force window to stay on top
+    app = QApplication([])  # Create a Qt application
+    filepath, _ = QFileDialog.getSaveFileName(None, title, initialdir, filetypes)
 
-    # Open the file dialog with the specified parameters
-    filenames = filedialog.askopenfilenames(title=title, initialdir=initialdir, filetypes=[filetypes])
-
-    # Destroy the root window after file selection
-    root.destroy()
-
-    # Convert the filenames to Path objects and return the sorted list
-    return sorted([Path(f) for f in filenames])
+    return filepath
 
 def scientificNotation(number, precision = None, exponent = None):
+    from symbols import math, typography
     
     oldNumberString = f'{number:e}' # use the "e" to ensure it is always formatted with an e
     oldCoefficient, oldExponent = oldNumberString.split('e') # get the coefficient and exponent from the old number string
