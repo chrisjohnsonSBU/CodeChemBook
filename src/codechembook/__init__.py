0a4b0b9841f6badb03c3ef31171993f7bd91d4b0
Ben Lear <135618964+ProfLear@users.noreply.github.com>
2024-12-20 15:15:32 -0500

    Update __init__.py
    renamed files.

--- src/codechembook/__init__.py
+++ src/codechembook/__init__.py
@@ -6,6 +6,5 @@ __version__ = "0.1.0"
 from .quickPlots import *
 #from .quickTools import *
 from .plotlyTemplates import *
-#from .chemtools import *
-from .numericaltools import *
+from .numericalTools import *
 from .symbols import *
